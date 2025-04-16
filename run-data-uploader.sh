4e9991a84908d3b2aaf37ab11a7f1a7cac381eef
lvhongbo <2918007@qq.com>
2025-04-16 21:48:34 +0800

    Add files via upload

--- run-data-uploader.sh
+++ run-data-uploader.sh
@@ -1,5 +1,7 @@
 docker run -d \
     --name data-uploader \
+    --network ai_network \
     -e TZ=Asia/Shanghai \
     -v /home/aideploy/pcba-ai/workroot/app_configs/data-uploader/appsettings.json:/app/appsettings.json \
+    --endpoint /bin/bash \
     data-uploader
